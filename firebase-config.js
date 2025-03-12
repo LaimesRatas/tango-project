b96ee8c20dfbda1100ae0680055c716179bbf351
GK <gkeraitis@gmail.com>
2025-03-12 22:45:04 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDTeH8iFH2bVxspr-E4fgABOu1ZrVPrIgc",
   authDomain: "tangochallenge-5b793.firebaseapp.com",
   projectId: "tangochallenge-5b793",
-  storageBucket: "tangochallenge-5b793.firebasestorage.app",
+  storageBucket: "tangochallenge-5b793.appspot.com",
   messagingSenderId: "124849282962",
   appId: "1:124849282962:web:5beecd57d9bf42c2932b4b",
   measurementId: "G-5H4YLH194H",
