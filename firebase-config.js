472a6d390733e806507b142adb3d0ac5e4f5b07b
GK <gkeraitis@gmail.com>
2025-03-12 16:30:46 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,11 +3,16 @@ const firebaseConfig = {
   apiKey: "AIzaSyDTeH8iFH2bVxspr-E4fgABOu1ZrVPrIgc",
   authDomain: "tangochallenge-5b793.firebaseapp.com",
   projectId: "tangochallenge-5b793",
-  storageBucket: "tangochallenge-5b793.firebasestorage.app",
+  storageBucket: "tangochallenge-5b793.appspot.com", // Pataisytas storage bucket pavadinimas
   messagingSenderId: "124849282962",
   appId: "1:124849282962:web:5beecd57d9bf42c2932b4b",
   measurementId: "G-5H4YLH194H",
   databaseURL: "https://tangochallenge-5b793-default-rtdb.europe-west1.firebasedatabase.app/"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Nustatome teisių reguliavimai duomenų rašymui
+// Tai leidžia rašyti duomenis be autentifikacijos
+firebase.database().ref().child('temporary_videos').onDisconnect().cancel();
