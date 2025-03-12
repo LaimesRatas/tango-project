d3f7a782b7c05d4b19d9c6b6197d8faa23de9d12
GK <gkeraitis@gmail.com>
2025-03-12 10:14:29 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,8 +6,8 @@ const firebaseConfig = {
   storageBucket: "tangochallenge-5b793.firebasestorage.app",
   messagingSenderId: "124849282962",
   appId: "1:124849282962:web:5beecd57d9bf42c2932b4b",
-  measurementId: "G-5H4YLH194H"
+  measurementId: "G-5H4YLH194H",
+  databaseURL: "https://tangochallenge-5b793-default-rtdb.europe-west1.firebasedatabase.app/"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
